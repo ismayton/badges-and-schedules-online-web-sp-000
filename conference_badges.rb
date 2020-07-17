9fda170d7b8f216e4329b38b20b3d4a558b5be26
Ian Mayton <ian.mayton@gmail.com>
2020-07-17 18:06:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 def assign_rooms(array)
   room_assignments = []
   room_number = 1
-  array.each do |name|
+  array.each_with_index do |name, index|
     assignment = "Hello, #{name}! You'll be assigned to room #{room_number}!"
     room_assignments << assignment
     room_number += 1
